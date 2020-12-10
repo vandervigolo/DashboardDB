d1e207e2bf8d8de3d129ac6a959b62fc9c1399aa
marcosmalfatti <marcosmalfatti@Wrk-Malfatti>
2020-12-10 18:26:02 -0200

    Criação do projeto Spring Boot inicial

---
+++ DashboardDB/src/main/java/br/com/dashboarddb/DashboardDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.dashboarddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardDbApplication.class, args);
+	}
+
+}
