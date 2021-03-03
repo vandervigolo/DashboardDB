112852bc93d0e578d789bd4feec73cbb36b2bb59
Marcos Malfatti <marcos.malfatti@gmail.com>
2021-03-02 21:54:39 -0300

    Remoção de Import não utilizado

--- DashboardDB/src/main/java/br/com/DashboardDB/Config/SpringMvcConfig.java
+++ DashboardDB/src/main/java/br/com/DashboardDB/Config/SpringMvcConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.thymeleaf.spring5.SpringTemplateEngine;
